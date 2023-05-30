b04e85443d8a5e3b0f2cbcdfed0bc670330cbf3a
Vishroy Seenarain <vishseenarain@gmail.com>
2023-05-30 12:03:55 +0000

    [DEV][Header] - Header created and styled.

--- src/components/common/button/button-cta.jsx
+++ src/components/common/button/button-cta.jsx
@@ -1,8 +1,20 @@
 import "./styles.scss";
+import "../../../assets/scss/animations.scss";
+import { useRef } from "react";
+import { useOnScreen } from "../../../hooks/use-on-screen/use-on-screen";
 
 const ButtonCta = () => {
+  const btnRef = useRef(null);
+  const isOnScreen = useOnScreen(btnRef);
+
+  if (isOnScreen) {
+    btnRef.current.querySelectorAll("a").forEach((element) => {
+      element.classList.add("animate");
+    });
+  }
+
   return (
-    <div className="block-cta">
+    <div ref={btnRef} className="block-cta">
       <a className="btn-cta">Contact</a>
     </div>
   );
